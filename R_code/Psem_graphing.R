dafdbbc1be80f4024392e1a52b4816fecc53419b
Jacob Herschberger <jakeberger92@gmail.com>
2025-04-09 12:48:10 -0400

    Updated PSEM plot function

--- R_code/Psem_graphing.R
+++ R_code/Psem_graphing.R
@@ -1,6 +1,6 @@
 library(qgraph)
 
-semGraph<-function(filename="test") {
+semGraph<-function(fit=fit) {
 edges<-summary(fit, fit=T,rsquare=T,conserve=T,standardize="none")$coefficients[,c("Response","Predictor","Estimate","P.Value")] %>% 
   filter(!grepl("~",Response)&!grepl("Loc",Predictor)) %>% 
   mutate(Response=case_when(grepl("herb", Response) ~ 'Herbivory',
@@ -65,8 +65,63 @@ qgraph(edge_list, layout = layout_matrix, labels = node_names, directed = TRUE,
        edge.labels = edge_labels, edge.color = edge_colors, edge.width = edge_widths,
        lty = edge_lty, curve = curves, edge.label.position = 0.7,
        edge.label.bg = "white",curveShape = -1.5,
-       filetype="jpg",
-       filename = filename,
-       height = 9, width = 9,
+       #filetype="jpg",
+       #filename = filename,
+       #height = 9, width = 9,
        shape="rectangle",vsize2=5,vsize=15,label.scale=F,border.width=2.1)  # Enlarged labels with background
+}
+
+
+semGraph_ggraph <- function(fit) {
+  library(ggraph)
+  library(tidygraph)
+  library(igraph)
+  library(dplyr)
+  library(ggplot2)
+  
+  edges <- summary(fit, fit = TRUE, rsquare = TRUE, conserve = TRUE, standardize = "none")$coefficients[, c("Response", "Predictor", "Estimate", "P.Value")] %>% 
+    filter(!grepl("~", Response) & !grepl("Loc", Predictor)) %>%
+    mutate(
+      Response = case_when(
+        grepl("herb", Response) ~ 'Herbivory',
+        grepl("Conc", Response) ~ 'Glycoalkaloids',
+        grepl("SLA", Response) ~ 'SLA',
+        grepl("Trichomes", Response) ~ 'Trichomes',
+        .default = Response
+      ),
+      Predictor = case_when(
+        Predictor == 'Clim_ave_PC1_sc' ~ 'Climate',
+        Predictor == 'Clim_PC1_sq_sc' ~ 'Climate (sq)',
+        grepl("Conc", Predictor) ~ 'Glycoalkaloids',
+        grepl("SLA", Predictor) ~ 'SLA',
+        grepl("Trichomes", Predictor) ~ 'Trichomes',
+        .default = Predictor
+      )
+    )
+  
+  # Create tidygraph
+  graph <- tbl_graph(edges = edges, directed = TRUE)
+  
+  # Manual layout as data.frame
+  layout_df <- data.frame(
+    x = c(0, -1, 0, 1, -0.5, 0.5),
+    y = c(1, 0, 0, 0, -1, -1),
+    name = c("Herbivory", "Glycoalkaloids", "SLA", "Trichomes", "Climate", "Climate (sq)")
+  )
+  
+  # Merge node positions into graph layout
+  ggraph(graph, layout = "manual", node.positions = layout_df) +
+    geom_edge_link(aes(
+      edge_alpha = abs(Estimate),
+      edge_width = abs(Estimate),
+      edge_linetype = P.Value < 0.05,
+      edge_color = Estimate > 0
+    ),
+    show.legend = FALSE,
+    arrow = arrow(length = unit(4, "mm"))
+    ) +
+    geom_node_label(aes(label = name), size = 4, label.padding = unit(0.2, "lines")) +
+    scale_edge_color_manual(values = c("darkred", "black")) +
+    scale_edge_linetype_manual(values = c("dashed", "solid")) +
+    theme_void()
 }
