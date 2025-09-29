53cb0d48d7d39c61e394febb4365a8c59017b835
Jacob Herschberger <jakeberger92@gmail.com>
2025-09-29 17:26:00 -0400

    Update code for data cleaning

--- R_code/Data_wrangling.R
+++ R_code/Data_wrangling.R
@@ -13,9 +13,11 @@ library(geodata)
 library(sf)
 library(terra)
 library(dismo)
+library(car)
 
 conflict_prefer("select", "dplyr")
 conflict_prefer("extract", "raster")
+conflict_prefer("filter", "dplyr")
 
 # Climate data ----
 con <-dbConnect(SQLite(), 'Data/Data.db') # Connect to the database
@@ -79,5 +81,107 @@ Clim_ave<-biovari %>%
 
 dbWriteTable(con,"Bioclims_1970_ave")
 
-# Glycoalkaloid concentration calculations ----
+# Glycoalkaloid concentration calculations Field 2022----
 
+## standard curve ----
+
+Stand<-dbReadTable(con,"Glycoalk_Spring_2023_standard_curve") %>% 
+  drop_na(Conc..mg.mL)
+
+
+
+lm1 <- Stand %>% 
+  mutate(Conc..mg.mL = as.numeric(Conc..mg.mL))  %>% 
+  lm(Abs~Conc..mg.mL, data=.) # Absorbence per mg/mL 
+
+Anova(lm1, type=2)
+summary(lm1) 
+
+hist(lm1$residuals)
+plot(lm1$residuals~lm1$fitted.values)
+abline(h=0) 
+
+qqPlot(lm1$residuals)
+
+Stand %>% 
+  ggplot(.,aes(x=Conc..mg.mL,y=Abs))+
+  geom_point()+
+  geom_smooth(method = "lm")
+
+## Total glycoalkaloid ----
+Glyco<-dbReadTable(con,"Glycoalk_Spring_2023_abs")%>% 
+  dplyr::select(!c(Notes,rep))%>% 
+  group_by(Sample_Label) %>% 
+  summarise(Abs=mean(Abs,na.rm=T))
+
+weight<-dbReadTable(con,"Glycoalk_Spring_2023_weight")%>% 
+  dplyr::select(!Notes)
+
+field_total_glyco_weight<-full_join(Glyco,weight) %>% 
+  dplyr::select(Sample_Label,Abs,Weight,Collect_Label) %>% 
+  mutate(Conc=(Abs-lm1$coefficients[1])/lm1$coefficients[2]*0.5/0.3*1.5/Weight) %>% 
+  drop_na() %>% mutate(
+    Plant_ID=Collect_Label,
+    Year=2022,
+    Time="2022",
+    loc="Field"
+  ) %>% select(Plant_ID,Time,Year,Conc)
+# (y-b)/slope gives the glycoalkaloid conc in samples
+# 1.5 mL is the total volume of the sample before transferring
+# I transferred 0.3 mL
+# 5 mL is the volume used before final step
+#group_by(Latitude,pop1) %>% 
+#summarise(Conc=mean(Conc,na.rm = T)) %>% 
+
+# 2023 glycoalkaloids ----
+## standard curve ----
+
+Plate<-dbReadTable(con,"Glycoalk_fall_2023_blank_plates") %>% 
+  select(Well,Plate,Abs) %>% rename(.,Blank_ABS=Abs)
+
+Stand<-dbReadTable(con,"Glycoalk_fall_2023_Standard_curve") %>% 
+  left_join(Plate) %>% mutate(Abs=(Abs-Blank_ABS)) 
+
+
+
+
+
+lm1 <- Stand %>% 
+  lm(Abs~Conc..mg.mL., data=.)
+
+Anova(lm1, type=2)
+summary(lm1) 
+
+hist(lm1$residuals)
+plot(lm1$residuals~lm1$fitted.values)
+abline(h=0) 
+
+qqPlot(lm1$residuals)
+
+Stand %>% 
+  ggplot(.,aes(x=Conc..mg.mL.,y=Abs))+
+  geom_point()+
+  geom_smooth(method = "lm")
+
+## Total glycoalkaloid ----
+Samples<-dbReadTable(con,"Glycoalk_fall_2023_Samples") %>% 
+  left_join(Plate) %>% 
+  filter(Sample!="Blank1",Abs!="NA") %>% 
+  mutate(Abs=(as.numeric(Abs)-Blank_ABS)) %>% 
+  left_join(
+    dbReadTable(con,"Glycoalk_fall_2023_Weight_data")
+    ) %>% 
+  mutate(Sample=as.numeric(Sample))
+
+Total_glyc<-Samples %>% 
+  group_by(.,Plant_ID,Leaf_location) %>% 
+  reframe(Abs=mean(Abs),
+          Leaf_weight=unique(Leaf_weight),
+          Sample=unique(Sample),
+          Experiement=unique(Experiement)) %>% 
+  mutate(Conc=as.numeric((Abs-lm1$coefficients[1])/lm1$coefficients[2]*0.5/0.3*1.5/Leaf_weight*1000)) %>% 
+  ungroup()
+# (y-b)/slope gives the glycoalkaloid conc in samples
+# 1.5 mL is the total volume of the sample before transferring
+# I transferred 0.3 mL
+# 5 mL is the volume used before final step
